8a46cbacc2bbf8714703c9898cf2a8c6a67a9942
Nicholas Lindley <me@thisoneplace.com>
2015-06-06 15:18:23 -0500

    Use streaming version of hapi-dust. Add regular stream test.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi');
 var Promise = require('promise');
 var Path = require('path');
+var PassThrough = require('stream').PassThrough;
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
@@ -15,14 +16,32 @@ server.route({
   handler: function(request, reply) {
     var deferredThing = new Promise(function(resolve, reject) {
       setTimeout(function() {
-        resolve("WOAH!!!");
-      }, 10000);
+        resolve("Delayed part");
+      }, 5000);
     });
 
     reply.view('index', {body: deferredThing});
   }
 });
 
+server.route({
+  method: 'GET',
+  path: '/stream',
+  handler: function(request, reply) {
+    var stream = new PassThrough();
+
+    stream.push("First chunk\n");
+    setTimeout(function() {
+      stream.push("Second chunk\n");
+    }, 2000);
+    setTimeout(function() {
+      stream.push("Third chunk\n");
+      stream.push(null);
+    }, 5000);
+
+    reply(stream);
+  }
+});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
